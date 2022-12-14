cb80477a8b073d281d239024fda5e404f7e44b85
Kyle OBrien <ob_3@ymail.com>
2022-12-14 11:04:21 -0700

    Gem Generator complete and values made editable to reflect real-time cost

--- src/tools/gemGenerator.js
+++ src/tools/gemGenerator.js
@@ -10,15 +10,22 @@ import { Link } from 'react-router-dom';
 
 const GemGenerator = () => {
     
-    const [type, setType] = useState("-");
+    const [type, setType] = useState("");
     const [carat, setCarat] = useState(0);
-    const [clarity, setClarity] = useState("-");
-    const [color, setColor] = useState("-");
-    const [cut, setCut] = useState("-");
+    const [clarity, setClarity] = useState("");
+    const [color, setColor] = useState("");
+    const [cut, setCut] = useState("");
     const [value, setValue] = useState(0);
     const [max, setMax] = useState(50);
 
-// Could input number into random function to limit the levels based on group's level
+    const gemTypes = ["Tourmaline", "Obsidian", "Aquamarine", "Tiger's Eye", "Diamond", "Citrine", "Amethyst", "Pyrite", "Sapphire", "Emerald", "Onyx", "Turquoise", "Ruby", "Opal", "Topaz", "Quartz", "Crocoite", "Azurite", "Rutile", "Tanzanite", "Jade", "Shungite", "Chryscocolla", "Jasper", "Bismuth", "Amber", "Whitestone"];
+
+    const gemClarity = ["FL", "VVS1", "VVS2", "VS1", "VS2", "SI1", "SI2", "SI3", "I1", "I2", "I3"];
+
+    const gemColor = ["D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+    const gemCut = ["Uncut", "Round", "Princess", "Oval", "Marquise", "Emerald", "Pear", "Asscher", "Cushion", "Trillion", "Radiant"];
+
     const random100 = () => {
         return Math.floor( Math.random() * max) + 1;
     }
@@ -118,29 +125,17 @@ const GemGenerator = () => {
     const clarityRand = () => {
         let clarityNum = random100();
         setValue((value) => value + (100 - clarityNum) * 3);
-        if(clarityNum === 1) {
-            setClarity("FL");
-        } else if (clarityNum <= 3) {
-            setClarity("VVS1");
-        } else if (clarityNum <= 6) {
-            setClarity("VVS2");
-        } else if (clarityNum <= 11) {
-            setClarity("VS1");
-        } else if (clarityNum <= 18) {
-            setClarity("VS2");
-        } else if (clarityNum <= 27) {
-            setClarity("SI1");
-        } else if (clarityNum <= 38) {
-            setClarity("SI2");
-        } else if (clarityNum <= 51) {
-            setClarity("SI3");
-        } else if (clarityNum <= 66) {
-            setClarity("I1");
-        } else if (clarityNum <= 83) {
-            setClarity("I2");
-        } else {
-            setClarity("I3");
-        }
+        if(clarityNum === 1) setClarity("FL");
+        else if (clarityNum <= 3) setClarity("VVS1");
+        else if (clarityNum <= 6) setClarity("VVS2");
+        else if (clarityNum <= 11) setClarity("VS1");
+        else if (clarityNum <= 18) setClarity("VS2");
+        else if (clarityNum <= 27) setClarity("SI1");
+        else if (clarityNum <= 38) setClarity("SI2");
+        else if (clarityNum <= 51) setClarity("SI3");
+        else if (clarityNum <= 66) setClarity("I1");
+        else if (clarityNum <= 83) setClarity("I2");
+        else setClarity("I3");
     }
     const colorRand = () => {
         let colorNum = random100();
@@ -194,7 +189,7 @@ const GemGenerator = () => {
         }
     }
     const cutRand = () => {
-        let cutNum = random100();
+        let cutNum = Math.floor( Math.random() * 100) + 1;
         if(cutNum <= 25) {
             setCut("Uncut");
             setValue((value) => value - 50);
@@ -227,36 +222,97 @@ const GemGenerator = () => {
         caratRand();
         colorRand();
         cutRand();
-        console.log(`${type} | ${clarity} | ${carat} | ${color} | ${cut} | $${value}`);
     }
-
+    
     const [modal, setModal] = useState(false);
 
     const toggle = () => setModal(!modal);
 
     const reset = () => {
-        setType("-");
+        setType("");
         setCarat(0);
-        setClarity("-");
-        setColor("-");
-        setCut("-");
+        setClarity("");
+        setColor("");
+        setCut("");
         setMax(50);
         setValue(0);
         toggle();
-    }
-
-    const valuer = (event) => {
-        const caratNum = event.target.value;
-        setValue((value) => value - (carat * 100));
-        setCarat(caratNum);
-        setValue((value) => value + (carat * 100));
+        console.clear();
     }
 
     const slider = (event) => {
         const sliderNum = event.target.value;
         setMax(sliderNum);
-        console.log(max);
     }
+    const typeValuer = (event) => {
+        const typeVal = event.target.value;
+        setType(typeVal);
+    }
+    const caratValuer = (event) => {
+        const caratVal = event.target.value;
+        setValue((value) => value - (carat * 100) + (caratVal * 100));
+        setCarat(caratVal);
+    }
+    const clarityValuer = (event) => {
+        const clarityVal = event.target.value;
+        function clarityNum(val) {
+            if (val === "FL") return 1;
+            else if (val === "VVS1") return 3;
+            else if (val === "VVS2") return 6;
+            else if (val === "VS1") return 11;
+            else if (val === "VS2") return 18;
+            else if (val === "SI1") return 27;
+            else if (val === "SI2") return 38;
+            else if (val === "SI3") return 51;
+            else if (val === "I1") return 66;
+            else if (val === "I2") return 83;
+            else if (val === "I3") return 90;
+            else return 100;
+        }
+        setValue((value) => value - ((100 - clarityNum(clarity)) * 3) + ((100 - clarityNum(clarityVal)) * 3));
+        setClarity(clarityVal);
+    }
+    const colorValuer = (event) => {
+        const colorVal = event.target.value;
+        function colorNum(val) {
+            if (val === "D") return 1;
+            else if (val === "E") return 3;
+            else if (val === "F") return 5;
+            else if (val === "G") return 8;
+            else if (val === "H") return 11;
+            else if (val === "I") return 14;
+            else if (val === "J") return 17;
+            else if (val === "K") return 21;
+            else if (val === "L") return 25;
+            else if (val === "M") return 30;
+            else if (val === "N") return 35;
+            else if (val === "O") return 40;
+            else if (val === "P") return 45;
+            else if (val === "Q") return 50;
+            else if (val === "R") return 55;
+            else if (val === "S") return 61;
+            else if (val === "T") return 67;
+            else if (val === "U") return 73;
+            else if (val === "V") return 79;
+            else if (val === "W") return 85;
+            else if (val === "X") return 91;
+            else if (val === "Y") return 97;
+            else if (val === "Z") return 99;
+            else return 100;
+        }
+        setValue((value) => value - ((100 - colorNum(color)) * 3) + ((100 - colorNum(colorVal)) * 3));
+        setColor(colorVal);
+    }
+    const cutValuer = (event) => {
+        const cutVal = event.target.value;
+        function cutNum(val) {
+            if (val === "Uncut") return 50;
+            else return 0;
+        }
+        setValue((value) => value + (cutNum(cut)) - (cutNum(cutVal)));
+        setCut(cutVal);
+    }
+    
 
     return (
         <div className="col-lg-4">
@@ -278,23 +334,43 @@ const GemGenerator = () => {
                         </div>
                         <div className='row'>
                             <h4 className='col text-start'>Stone: </h4>
-                            <input type="text" className='col' value={type} readOnly />
+                            <input list="gemTypes" type="text" className='col' value={type} onChange={typeValuer} />
+                            <datalist id="gemTypes">
+                                {gemTypes.map((e, key) => {
+                                    return <option key={key} value={e} />
+                                })}
+                            </datalist>
                         </div>
                         <div className='row'>
                             <h4 className='col text-start'>Carat: </h4>
-                            <input className='col' value={carat} onChange={valuer} />
+                            <input className='col' value={carat} onChange={caratValuer} />
                         </div>
                         <div className='row'>
                             <h4 className='col text-start'>Clarity: </h4>
-                            <input className='col' value={clarity} readOnly />
+                            <input list="gemClarity" className='col' value={clarity} onChange={clarityValuer} />
+                            <datalist id="gemClarity">
+                                {gemClarity.map((e, key) => {
+                                    return <option key={key} value={e} />
+                                })}
+                            </datalist>
                         </div>
                         <div className='row'>
                             <h4 className='col text-start'>Color: </h4>
-                            <input className='col' value={color} readOnly />
+                            <input list="gemColor" className='col' value={color} onChange={colorValuer} />
+                            <datalist id="gemColor">
+                                {gemColor.map((e, key) => {
+                                    return <option key={key} value={e} />
+                                })}
+                            </datalist>
                         </div>
                         <div className='row'>
                             <h4 className='col text-start'>Cut: </h4>
-                            <input className='col' value={cut} readOnly />
+                            <input list="gemCut" className='col' value={cut} onChange={cutValuer} />
+                            <datalist id="gemCut">
+                                {gemCut.map((e, key) => {
+                                    return <option key={key} value={e} />
+                                })}
+                            </datalist>
                         </div>
                         <div className='row'>
                             <h4 className='col text-start'>Value: </h4>
@@ -304,7 +380,7 @@ const GemGenerator = () => {
                 </ModalBody>
                 <ModalFooter>
                 <Button color="primary" onClick={gemRand} >
-                    Calculate
+                    Generate
                 </Button>{' '}
                 <Button color="secondary" onClick={reset}>
                     Cancel
